60099af833137d2cda6d7867fcd74559dbb6f7ff
HsinYuLee <laalee0525@gmail.com>
2018-09-14 11:17:02 +0800

    Implement play video function.

--- VideoPlayer/VideoPlayer/ViewController.swift
+++ VideoPlayer/VideoPlayer/ViewController.swift
@@ -7,19 +7,61 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var urlTextField: UITextField!
 
+    @IBOutlet weak var playButton: UIButton!
+    
+    @IBOutlet weak var videoView: UIView!
+    
+    var avPlayer: AVPlayer?
+    
+    var avPlayerLayer : AVPlayerLayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        playButton.isSelected = false
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func searchUrl(_ sender: UIButton) {
+        
+        guard let urlString = urlTextField.text else { return }
+        
+        guard let url = URL(string: urlString) else { return }
+        
+        avPlayer = AVPlayer(url: url)
+        
+        playVideo(playButton)
     }
-
+    
+    @IBAction func playVideo(_ sender: UIButton) {
+        
+        guard let player = avPlayer else { return }
+        
+        avPlayerLayer = AVPlayerLayer(player: player)
+        
+        avPlayerLayer.videoGravity = AVLayerVideoGravity.resize
+        
+        avPlayerLayer.frame = videoView.layer.bounds
+        
+        videoView.layer.addSublayer(avPlayerLayer!)
+        
+        playButton.isSelected = !playButton.isSelected
+        
+        if playButton.isSelected {
+            
+            player.play()
+            
+        } else {
+            
+            player.pause()
+        }
+    }
 
 }
 
